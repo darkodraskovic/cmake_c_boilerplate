21fa2c44160d99db70a316d92765eb63eb58d0ea
Darko Draskovic <darko.draskovic@gmail.com>
2022-05-29 17:39:11 +0200

    Add config option to build script
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-cd build/
-make
-cd ../bin
-./main
+CONFIGURE=false
+EXE_NAME=main
+COMPILE_CMD="make"
+ROOT_DIR=~/Development/chip8 # <- CHANGE
+
+while getopts ":c" option; do
+    case $option in
+        c)
+            CONFIGURE=true
+            ;;
+    esac
+done
+
+if [ $CONFIGURE == true ]; then
+    cd $ROOT_DIR/build/
+    cmake ..
+fi
+
+
+cd $ROOT_DIR/build/
+$COMPILE_CMD
+cd $ROOT_DIR/bin
+./$EXE_NAME
